221c84ac88cec9ce2ad80b64d79110b1a662bcf3
Vadim <55794804+Vadimsdl@users.noreply.github.com>
2020-10-06 12:00:11 +0300

    Not work

---
+++ Planetario/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Planetario
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(964, 573);
+            this.panel1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(988, 597);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "Planetario";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Planetario/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace Planetario
+{
+    class Player
+    {
+        uint    Id;             // Ид игрока
+        string  Nickname;       // Ник игрока
+        ulong   Score;          // Счёт игрока
+        ulong   Record;         // Рекорд игрока
+
+        Ellipse Collise; 
+
+    }
+    class CurrentPlayer
+    {
+        Player ThisPlayer;    // Текущий игрок
+
+        float MapScale;       // Текущий масштаб карты
+        float Speed;          // Текущая скорость игрока
+
+        bool BonusSpeed;       // Включено ли ускорение у игрока
+
+        uint LostScoresOnStep; // Количество очков которые мы теряем, например каждые 5 
+
+        Point Position;     // Позиция игрока
+        Color Color;        // Цвет игрока
+    }
+}
+
